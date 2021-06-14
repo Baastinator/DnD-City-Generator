541a277360ec4489f8cbea99078306e87ca8e259
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 19:51:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnD_City_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
